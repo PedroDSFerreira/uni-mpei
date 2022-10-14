320e45e81c0ab3c6d03a62a89c6a2347847c51a7
Pedro Ferreira <pedrodsf21@gmail.com>
2022-10-14 16:31:19 +0100

    Ex. 2 b) & c)

--- 2/PL2.m
+++ 2/PL2.m
@@ -3,9 +3,9 @@ clear all
 close all
 
 % Parte I
-%% Ex.1
+% Ex.1
 
-% a)
+%% a)
 
 
 S = 2;
@@ -17,11 +17,11 @@ success = sum(sons) >= k;
 'a)'
 sum(success)/sample_size
 
-% b)
+%% b)
 'b)'
 (2+1)/4
 
-% c)
+%% c)
 'c)'
 % P(A|B) = P(AnB)/P(B)
 
@@ -48,36 +48,59 @@ p_ab/p_b
 'Valor Teórico'
 1/(2+1)
 
-%% Ex. 2
+% Ex. 2
 %% a)
 n = 20;
 m = 100;
 
 sample_size = 1e4;
-
-for samples = 1:sample_size-1
-    count = 0;
-    for sample = round(rand(n, m)* 100)
-        if length(unique(sample)) == n
-            count = count + 1;
-        end  
+count = 0;
+for sample = randi(m, n, sample_size)
+    if length(unique(sample)) == n
+        count = count + 1;
     end
-    probs(samples) = count/n;
 end
-p = sum(probs)/sample_size
+p = count/sample_size
 
 %% b)
 n = 20;
 m = 100;
 
 sample_size = 1e4;
+
 count = 0;
-for samples = 1:sample_size-1
-    for sample = round(rand(n, m)* 100)
-        if length(unique(sample)) <= n-2
-            count = count + 1;
-            break
-        end  
+for sample = randi(m, n, sample_size)
+    if length(unique(sample)) < n-1
+        count = count + 1;
     end
 end
 p = count/sample_size
+
+%% c)
+
+
+sample_size = 1e5;
+
+
+probability = zeros(1,10);
+n = 10:10:100;
+m = [1000 100000];
+for i = 1:2
+    for j = 1:10
+        count = 0;
+        for sample = randi(m(i), n(j), sample_size)
+            if length(unique(sample)) < n(j)-1
+                count = count + 1;
+            end
+        end
+        probability(j) = count/sample_size;
+    end
+
+    subplot(2,1,i);
+    plot(n,probability);
+    legend(strcat('m=', num2str(m(i))));
+    xlabel('m');
+    ylabel('Probability');
+end
+
+
