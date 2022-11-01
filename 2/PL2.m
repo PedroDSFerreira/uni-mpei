1e98177908a541ace9e79c65cfda5379e79f95e2
Pedro Ferreira <pedrodsf21@gmail.com>
2022-11-01 17:22:43 +0000

    Update assignment 2

--- 2/PL2.m
+++ 2/PL2.m
@@ -3,9 +3,7 @@ clear all
 close all
 
 % Parte I
-% Ex.1
-
-%% a)
+%% Ex.1 - a)
 
 
 S = 2;
@@ -48,8 +46,8 @@ p_ab/p_b
 'Valor Teórico'
 1/(2+1)
 
-% Ex. 2
-%% a)
+
+%% Ex. 2 - a)
 n = 20;
 m = 100;
 
@@ -124,9 +122,7 @@ legend(strcat('n=', num2str(n)));
 xlabel('m');
 ylabel('Probability');
 
-
-% Ex. 3
-%% a)
+%% Ex. 3 - a)
 k = 10; % number of keys
 T = 1000; % array size
 
@@ -180,16 +176,12 @@ legend(strcat('k=', num2str(k)));
 xlabel('T');
 ylabel('Probability');
 
-
-% Ex. 4 - TODO
-%% a)
+%% Ex. 4 - a) - TODO
 p = 0.5;
 d = 365; % days
 sample_size = 1e4;
 
-
-% Ex. 7
-%% a)
+%% Ex. 7 - a)
 % Teórica
 
 % P(ser do Carlos | tem um bug) = P(ser do Carlos n tem um bug)/P(tem um
@@ -208,7 +200,6 @@ pbug = 0.01*0.2 + 0.05*0.3 + 0.001*0.5;
 % bug)
 probC = pCbug/pbug
 
-
 % b)
 pAbug = 0.01*0.2;
 probA = pAbug/pbug
@@ -218,4 +209,95 @@ probB = pBbug/pbug
 
 
 % Parte II
-% Ex. 2
+%% Ex. 1 - a)
+pX = ones(1,6)/6;
+xi = 1:6;
+
+stem(xi, pX);
+ylabel('pX');
+xlabel('xi');
+
+%% b)
+FX = cumsum([0 0 pX 0 0]);
+stairs(-1:8, FX);
+  
+%% Ex. 2 - a)
+x = 1:100;
+px = ones(1,100)/100;
+subplot (2, 1, 1);
+stem(x, px), xlabel('x'), ylabel('P(x)');
+
+%% b)
+xi = [5 50 100];
+cFav = [90 9 1];
+Px = cFav/100;
+
+%% c)
+subplot (2, 1, 2);
+Fx = cumsum([0 0 pNota 0 0]);
+stairs(-1:102, Fx);
+
+%% Ex. 4 (REALIZADO EM GRUPO)
+N = 1e5;
+p = 0.3;
+n = 5; % num de peças
+
+xi = 0:5;
+pX = zeros(1,5);
+amostras = rand(n,N) <= p;
+
+for k=0:5
+    sucesso = sum(amostras)==k;
+    pX(k+1) = sum(sucesso)/N;
+end
+
+FX = cumsum([0 pX 0]);
+stairs(-1:6, FX);
+
+Prob = sum(sum(amostras)<=2)/N;
+
+%% Ex. 6 - a) (REALIZADO EM GRUPO)
+p = 1/1000; % probabilidade
+n = 8000; % espaço de amostragem - chips
+k = 7; % defeitos
+% E[x] = n*p = 8
+
+% Poisson
+lambda=n*p;
+pX1 = ((lambda^k)/factorial(k))*(exp(-lambda));
+
+%% b) (REALIZADO EM GRUPO)
+% Binomial
+% pX2 = factorial(n)/(factorial(n-k)*factorial(k)*p^k*(1-p)^(n-k));
+pX2 = prod(n:-1:n-k+1)/prod(1:k)*p^k*(1-p)^(n-k);
+
+%% Ex. 7 - a) (REALIZADO EM GRUPO)
+n = 1;
+p = 15;
+k = 0;
+lambda = p * n;
+pk = lambda^k * exp(-lambda) / factorial(k);
+
+%% b) (REALIZADO EM GRUPO)
+n = 1;
+p = 15;
+k = 10;
+lambda = p * n;
+x = 0;
+
+for i = 0 : k
+    x = x + lambda^i * exp(-lambda) / factorial(i);
+end
+p = 1-x
+
+%% Ex. 8
+lambda = 0.02; n = 100;
+
+lambda = lambda * n;
+pk = 0;
+
+for k = 0 : 1
+    pk = pk + lambda^k * exp(-lambda) / factorial(k);
+end
+
+pk
