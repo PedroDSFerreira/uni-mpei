f2f86cd0c32b16f8bed1a71954f1d749794c3e40
Pedro Ferreira <pedrodsf21@gmail.com>
2022-11-23 22:05:02 +0000

    Update assignment 3

--- 3/PL3.m
+++ 3/PL3.m
@@ -450,6 +450,7 @@ Q = [
     ];
 F = inv((eye(3)-Q))
 
+% numero de passos antes da absorçao, começando no estado x
 t = sum(F); % F'*ones(3, 1)
 t(1)
 t(2)
@@ -461,14 +462,54 @@ Q = [
     0.2 0.6 0.2;
     0.0 0.1 0.4;
     ];
-F = inv((eye(3)-Q));
+F = inv((eye(3)-Q)); % Matriz fundamental
 
 R = [
     0.0 0.3 0.0;
     0.0 0.0 0.1
     ];
 
-B = R*F;
+B = R*F; % Probabilidade de absorção
 
 B(1,1)
 B(2,1)
+
+%% Ex. 7 - a)
+
+A = [
+    
+    ]
+
+% Anexo 
+function [state] = crawl(H, first, last)
+% the sequence of states will be saved in the vector "state"
+% initially, the vector contains only the initial state:
+    state = [first];
+% keep moving from state to state until state "last" is reached:
+    while (1)
+        state(end+1) = nextState(H, state(end));
+        if ismember(state(end), last) % verifies if state(end) is absorbing
+            break;
+        end
+    end
+end
+% Returning the next state
+% Inputs:
+% H - state transition matrix
+% currentState - current state
+function state = nextState(H, currentState)
+% find the probabilities of reaching all states starting at the current one:
+    probVector = H(:,currentState)'; % probVector is a row vector
+    n = length(probVector); %n is the number of states
+% generate the next state randomly according to probabilities probVector:
+    state = discrete_rnd(1:n, probVector);
+end
+% Generate randomly the next state.
+% Inputs:
+% states = vector with state values
+% probVector = probability vector
+function state = discrete_rnd(states, probVector)
+    U=rand();
+    i = 1 + sum(U > cumsum(probVector));
+    state= states(i);
+end
