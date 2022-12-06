3e1adb2e02de017c007ee62d69747b826774af82
Pedro Ferreira <pedrodsf21@gmail.com>
2022-12-06 20:07:02 +0000

    Ex. 3 a) i)

--- 4/PL4.m
+++ 4/PL4.m
@@ -37,82 +37,77 @@ table_sizes = [5e5 1e6 2e6];
 
 simulation_1 = cell(3);
 for i=1:length(table_sizes)
-    hashtable = cell(1, table_sizes(i));
-    colisions = 0;
+    indexes = zeros(1, N);
     tic
     for j=1:N
         hashcode = string2hash(keys{j}, 'djb2');
         index = abs(mod(hashcode, table_sizes(i)))+1;
-        if isempty(hashtable{index})
-            hashtable{index} = hashcode;
-        else
-            colisions = colisions + 1;
-            hashtable{index}(end+1) = hashcode;
-        end
+
+        indexes(j) = index;
     end
-    simulation_1{i, 1} = hashtable;
-    simulation_1{i, 2} = colisions;
+    simulation_1{i, 1} = indexes;
+    simulation_1{i, 2} = length(indexes)-length(unique(indexes));
     simulation_1{i, 3} = toc;
 end
 
 
 simulation_2 = cell(3);
 for i=1:length(table_sizes)
-    hashtable = cell(1, table_sizes(i));
-    colisions = 0;
+    indexes = zeros(1, N);
     tic
     for j=1:N
         hashcode = string2hash(keys{j}, 'sdbm');
         index = abs(mod(hashcode, table_sizes(i)))+1;
-        if isempty(hashtable{index})
-            hashtable{index} = hashcode;
-        else
-            colisions = colisions + 1;
-            hashtable{index}(end+1) = hashcode;
-        end
+        
+        indexes(j) = index;
     end
-    simulation_2{i, 1} = hashtable;
-    simulation_2{i, 2} = colisions;
+    simulation_2{i, 1} = indexes;
+    simulation_2{i, 2} = length(indexes)-length(unique(indexes));
     simulation_2{i, 3} = toc;
 end
 
 simulation_3 = cell(3);
 seed = 123;
 for i=1:length(table_sizes)
-    hashtable = cell(1, table_sizes(i));
-    colisions = 0;
+    indexes = zeros(1, N);
     tic
     for j=1:N
         hashcode = DJB31MA(keys{j}, seed);
         index = abs(mod(hashcode, table_sizes(i)))+1;
-        if isempty(hashtable{index})
-            hashtable{index} = hashcode;
-        else
-            colisions = colisions + 1;
-            hashtable{index}(end+1) = hashcode;
-        end
+        
+        indexes(j) = index;
     end
-    simulation_3{i, 1} = hashtable;
-    simulation_3{i, 2} = colisions;
+    simulation_3{i, 1} = indexes;
+    simulation_3{i, 2} = length(indexes)-length(unique(indexes));
     simulation_3{i, 3} = toc;
 end
 
 simulation_4 = cell(3);
 for i=1:length(table_sizes)
-    hashtable = cell(1, table_sizes(i));
-    colisions = 0;
+    indexes = zeros(1, N);
     tic
     for j=1:N
         hashcode = hashstring(keys{j}, table_sizes(i));
         index = hashcode+1;
-        if isempty(hashtable{index})
-            hashtable{index} = hashcode;
-        else
-            colisions = colisions + 1;
-            hashtable{index}(end+1) = hashcode;
-        end
+        
+        indexes(j) = index;
     end
-    simulation_4{i, 1} = hashtable;
-    simulation_4{i, 2} = colisions;
+    simulation_4{i, 1} = indexes;
+    simulation_4{i, 2} = length(indexes)-length(unique(indexes));
     simulation_4{i, 3} = toc;
+end
+
+%% Ex. 3
+% a)
+% i)
+for i=1:3
+    figure()
+    subplot(4,1,1)
+    histogram(simulation_1{i, 1}, 100)
+    subplot(4,1,2)
+    histogram(simulation_2{i, 1}, 100)
+    subplot(4,1,3)
+    histogram(simulation_3{i, 1}, 100)
+    subplot(4,1,4)
+    histogram(simulation_4{i, 1}, 100)
 end
