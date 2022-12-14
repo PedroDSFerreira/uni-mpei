d5c90b24a2c2fd7fbdbd2741c4723d9b714234d4
Pedro Ferreira <pedrodsf21@gmail.com>
2022-12-14 10:58:20 +0000

    Update bloom filter functions

--- 4/insert_bloom_filter.m
+++ 4/insert_bloom_filter.m
@@ -6,12 +6,11 @@ assert(length(seeds)==k);
 N = length(bloom_filter);
 
 % Generate k hashes using element
-hashes = zeros(1, k);
 for i=1:k
-    hashes(i) = DJB31MA(element, seeds(i));
+    hash = DJB31MA(element, seeds(i));
     
     % From hash, create index to store in bloom filter
-    index = abs(mod(hashes(i), N))+1;
+    index = abs(mod(hash, N))+1;
     bloom_filter(index) = 1;
 end
 
